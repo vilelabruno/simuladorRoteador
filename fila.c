14c1b9c19e503a2e3001c40935c2f4d6df1fc86f
2016.1.08.042 <2016.1.08.042@bcc-p135263>
2019-05-17 17:54:36 -0300

    last friday changes

--- fila.c
+++ fila.c
@@ -1,42 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
-#include "fila.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include"fila.h"
 
 pacote * aloca_pct(){
-    return malloc (sizeof(pacote*));
+	return (malloc(sizeof(pacote)));
 }
 
 /**
  * 
- * @param fim points to the last item of the array
- * @param tamanho of the package
- * @return 0 if error, 1 if not error
+ * @param inicio aponta o primeiro da fila
+ * @param fim aponta o ultimo da fila
+ * @param tamanho do pacote a ser inserido
+ * @return 1 caso insira, 0 caso erro
  */
+int inserir(pacote ** inicio, pacote ** fim, double tamanho){
+	//fila vazia
+	if(*inicio == NULL){
+		*inicio = aloca_pct();
+		if(*inicio == NULL){
+			return 0;
+		}
+		*fim = *inicio;
+		(*inicio)->tamanho = tamanho;
+		(*inicio)->prox = NULL;
+		return 1;
+	}else{
+		pacote * tmp = aloca_pct();
+		if(tmp == NULL){
+			return 0;
+		}
+		tmp->tamanho = tamanho;
+		tmp->prox = NULL;
+		
+		(*fim)->prox = tmp;
+		(*fim) = tmp;
+		return 1;
+	}
+}
 
 
-int insere(pacote * inicio, pacote * fim, double tamanho){
-    if (inicio == NULL){
-        inicio = aloca_pct();
-        if (inicio == NULL) return 0;
-        fim = inicio;
-        inicio->tamanho = tamanho;
-        inicio->prox = NULL;
-        return 1;
-    }else{
-        pacote * tmp  = aloca_pct();
-        if (tmp == NULL) return 0;
-        tmp->tamanho = tamanho;
-        tmp->prox = NULL;
-        fim->prox = tmp;
-        fim = tmp;
-        return 1;
-    }
-   return 0; 
+/**
+ * 
+ * @param inicio da fila
+ * @return 0 caso fila vazia, tamanho do pct caso contrario
+ */
+double remover(pacote ** inicio){
+	if(*inicio == NULL){
+		return 0;
+	}else{
+		pacote * tmp = *inicio;
+		double tamanho = (*inicio)->tamanho;
+		(*inicio) = (*inicio)->prox;
+		free(tmp);
+		return tamanho;
+	}
 }
